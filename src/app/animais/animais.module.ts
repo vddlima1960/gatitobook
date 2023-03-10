b4295d49fbc6fbceaab75e90ac559550b12da4f6
vvdlima1960 <vddlimaredhat@gmail.com>
2023-03-10 07:25:20 -0300

    interceptor

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { AnimalComponent } from './animal/animal.component';
+import { CartaoModule } from '../componentes/cartao/cartao.module';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 
 @NgModule({
-  declarations: [ListaAnimaisComponent],
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
   imports: [
     CommonModule,
-    AnimaisRoutingModule
+    AnimaisRoutingModule,
+    CartaoModule
   ]
 })
 export class AnimaisModule { }

f77092c0e68c7719011a040e9b2f3784f69a9eac
vvdlima1960 <vddlimaredhat@gmail.com>
2023-03-11 07:39:07 -0300

    criando resolver

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { HomeComponent } from './home.component';
 
 import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule, ReactiveFormsModule ],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, SharedModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
